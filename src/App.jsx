ef476ed34a123e36489f977b59c5776e0ca508b2
Safdar-Ali-India <safdarali63166@gmail.com>
2025-11-01 03:34:09 +0530

    style: new app css written

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState, useCallback } from "react";
-
-
- 
+import { useState, useCallback } from "react"
 
 function App() {
- const [length, setLength] = useState(10);
- const [numberAllowed, setNumberAllowed] =useState(false);
- const [specialCharAllowed, setSpecialCharAllowed] =useState(false);
+  const [length, setLength] = useState(10)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [specialCharAllowed, setSpecialCharAllowed] = useState(false)
 
- const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("")
 
- const generatePassword = useCallback( () => {}, [length,numberAllowed, specialCharAllowed, setPassword ] )  
+  const generatePassword = useCallback(() => {}, [length, numberAllowed, specialCharAllowed, setPassword])
 
   return (
     <>
-     <h1 className="text-white text-center text-4xl">Safdar's Password Generator</h1>
+      <h1 className="text-white text-center text-4xl">Safdar's Password Generator</h1>
     </>
   )
-} 
- 
+}
+
 export default App
