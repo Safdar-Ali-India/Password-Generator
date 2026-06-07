c65c8c2cf674c473546e97fd3a21fa479f9517f3
Safdar-Ali-India <safdarali63166@gmail.com>
2026-06-08 02:28:43 +0530

    feat : external links for footer and coffee

---
+++ src/lib/links.js
@@ -0,0 +1,7 @@
+export const EXTERNAL_LINKS = {
+  coffee: "https://buymeacoffee.com/safdarali",
+  youtube: "https://www.youtube.com/@safdarali_?sub_confirmation=1",
+  github: "https://github.com/Safdar-Ali-India",
+  linkedin: "https://www.linkedin.com/in/safdarali25/",
+  twitter: "https://x.com/safdarali___",
+}
